1cc4e2b2600d76d3ea5ef2fb72711651c7cbc544
mohamedeldony3 <60990149+mohamedeldony3@users.noreply.github.com>
2024-08-09 16:37:39 -0400

    Update احا.js

--- plugins/احا.js
+++ plugins/احا.js
@@ -1,16 +1,15 @@
-.gps cc|import fs from 'fs';
+import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
-
   const vn = 'https://f.uguu.se/EFiiWDly.mp3';
   const chat = global.db.data.chats[m.chat];
-  
-  if (/^احا$/i.test(m.text) && !chat.isBanned) {
-  
+  //const s = seconds: '4556'
+  // const estilo = { key: {  fromMe: false, participant: 0@s.whatsapp.net, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
+  // const estiloaudio = { key: {  fromMe: false, participant: 0@s.whatsapp.net, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
+  if (/^ا|احا|ا/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-    
+    // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
   }
   return !0;
 };
